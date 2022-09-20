2e863fff1edb5c3dc20fec1e4798bf5ffd507e71
Vikas1028 <vikasbhandekar2018@gmail.com>
2022-09-20 19:54:28 +0530

    default value of all primitive data types of Java

---
+++ Primitive_Data_Type.java
@@ -0,0 +1,24 @@
+public class Primitive_Data_Type {
+	
+	static byte Number1;
+	static short Number2;
+    static int Number3;
+    static long Number4;
+    static float Number5;
+    static double Number6;
+    static boolean value;
+    static String name;
+    
+    public static void main(String[] args) {
+    	System.out.println("Default value of Primitive Data Type ");
+    	System.out.println("Byte : " + Number1);
+    	System.out.println("Short : " + Number2);
+    	System.out.println("Int : " + Number3);
+    	System.out.println("Long : " + Number4);
+    	System.out.println("Float : " + Number5);
+    	System.out.println("Double : " + Number6);
+    	System.out.println("Boolean : " + value);
+    	System.out.println("String : " + name);
+    	}
+	
+}
