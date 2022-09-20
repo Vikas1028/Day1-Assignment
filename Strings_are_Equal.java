e6a76502f233a9f9f039c1c5216f3e37ddb1953d
Vikas1028 <vikasbhandekar2018@gmail.com>
2022-09-20 20:02:55 +0530

    check two strings are equal or not

---
+++ Strings_are_Equal.java
@@ -0,0 +1,14 @@
+public class Strings_are_Equal {
+
+	public static void main(String[] args) {
+		String name1 = "Vikas";
+		String name2 = "Ritik";
+		System.out.println("Name1: " + name1);
+		System.out.println("Name2: " + name2);
+		if (name1 == name2) {
+			System.out.println("Name1 String and Name2 String are Equal");
+		} else {
+			System.out.println("Name1 String and Name2 String are NotEqual");
+		}
+	}
+}
